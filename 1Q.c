cc15c3d3f08bff949a9c7083fbfd530ab7faf540
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-09-23 10:29:04 +0530

    Create 1Q.c

---
+++ 1Q.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+    int main(){
+        int n;
+        printf("Enter any number : ");
+        scanf("%d",&n);
+        printf(n>0?"The number is positive":"The number is non-positive");
+        return 0;
+    }
